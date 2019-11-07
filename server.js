ee50ae217289ea4632bb0ae0631aa602d273c8bf
gastonvejar <gaston.vejar@gmail.com>
2019-11-07 23:10:33 +0100

    Fixed SQL Injection with prepared statements

--- server.js
+++ server.js
@@ -21,12 +21,36 @@ app.get('/invalidLogin.html', function (req, res) {
    res.sendFile( __dirname + "/" + "invalidLogin.html" );
 })
 
+/* ORIGINAL VULNERABLE CODE
+
 app.get('/process_get', function (req, res) {
 
-   //this is the SQL injection vulnerable query
    let sql = 'SELECT * FROM users WHERE email = \'' + req.query.username + '\' AND password = \'' + req.query.password + '\'';
    console.log(sql);
    db.all(sql, [], (err, rows) => {
+      if(err) {
+         console.log(err);
+         return callback(err);
+      }
+
+      //if the query returns results it will show the login information
+      if(rows.length > 0){
+         console.log('Logged in as:' + rows[0].email + ' - Password: ' + rows[0].password);
+         res.send('LOG-IN SUCCESSFUL! Logged in as:' + rows[0].email + ' - Password: ' + rows[0].password);
+      }
+
+      //if the query returns no results it will show the invalid login page
+      else {
+         res.redirect('http://127.0.0.1:8081/invalidLogin.html');
+      }
+   });   
+})*/
+
+app.get('/process_get', function (req, res) {
+
+   //Fixed vulnerability with prepared statements
+
+   db.all('SELECT * FROM users WHERE email = ? AND password = ?', [req.query.username, req.query.password], (err, rows) => {
       if(err) {
          console.log(err);
          return callback(err);
